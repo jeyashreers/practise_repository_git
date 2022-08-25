6bd84548a7e8ca7e12399cbf41178469a8f94bb5
EC2 Default User <ec2-user@ip-172-31-4-74.us-west-2.compute.internal>
2022-08-25 05:21:08 +0000

    changing age.sh

--- age.sh
+++ age.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+!/bin/bash
 echo "enter your birth year"
 read year
 echo "enter your birth month"
 read month
 cyear=$(date '+%Y')
-cmonth=$(date'+%m')
+cmonth=$(date '+%m')
 agey=`expr $cyear - $year`
-agem='expr $cmonth -$month'
+agem=`expr $cmonth -$month`
 if [ $agem -lt 0 ];
 then
 agem=`expr $cmonth - $month + 12`
 else
-agem='expr $cmonth -$month'
+agem=`expr $cmonth - $month`
 fi
 echo "The current age is $agey years and $agem months"
